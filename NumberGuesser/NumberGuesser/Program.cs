1906f2aa75930506f8257f87a95310c30b857195
Jana <janarthan.paramanandam@ge.com>
2019-09-02 17:59:32 +0530

    Add NumberGuesser

---
+++ NumberGuesser/NumberGuesser/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace NumberGuesser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int fn = random.Next(2, 10);
+            int sn = random.Next(2, 10);
+            int subtraction = random.Next(2, 10);
+            int answer = fn * sn - subtraction;
+
+            String prompt = "... Press enter when ready";
+
+            Console.WriteLine("Think of a number between 1 and 10" + prompt);
+            Console.ReadKey();
+
+            Console.WriteLine("Multiply your number by " + fn + prompt);
+            Console.ReadKey();
+
+            Console.WriteLine("Multiply the result by " + sn + prompt);
+            Console.ReadKey();
+
+            Console.WriteLine("Divide the result by the number you originally thought of " + prompt);
+            Console.ReadKey();
+
+            Console.WriteLine("Subtract " + subtraction + prompt);
+            Console.ReadKey();
+
+            Console.WriteLine("The answer is " + answer);
+
+        }
+    }
+}
